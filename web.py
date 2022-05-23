87e224c03d47232c9ecbc1f17b2a6e85875372de
Matthew Hyatt <mhyatt000@gmail.com>
2022-05-23 12:33:37 -0400

    deploy

--- web.py
+++ web.py
@@ -1,18 +1,45 @@
 import requests
-import BeautifulSoup as bs4
+from bs4 import BeautifulSoup
 import re
 
 def scrape_model_hub(url):
+    '''TF hub, Pytorch hub, modelhub, huggingface'''
+
+    hub = url.split('/')[2].split('.')[0]
+
+    print(hub)
+
+    page = requests.get(url)
+    content = page.content
+
+    soup = BeautifulSoup(content, 'html.parser')
+    # print(soup.prettify())
+
+    body = soup.find('body')
 
     blob = ''
 
-    if 'hub_a':
+    if hub == 'huggingface':
         pass
 
-    if 'hub_b':
-        pass
+    if hub == 'tfhub':
+
+        desc = soup.find_all(class_='overview')
+        print(body.prettify())
+
 
     if 'hub_c':
         pass
 
     return blob
+
+
+def main():
+
+    hub = 'https://tfhub.dev/deepmind/mmt/architecture-ft_language-q-24/1'
+    # hub = 'https://huggingface.co/docs/transformers/main/en/model_doc/gpt2#transformers.GPT2LMHeadModel'
+
+    scrape_model_hub(hub)
+
+if __name__ == '__main__':
+    main()
